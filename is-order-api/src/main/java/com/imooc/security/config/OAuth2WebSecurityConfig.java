099eeeeeb7fdac22452c954b241065680d1de1b8
usernameUndefind <1019922870@qq.com>
2021-03-07 12:32:25 +0800

    token和client客户端持久化存储 token转换为user对象

--- is-order-api/src/main/java/com/imooc/security/config/OAuth2WebSecurityConfig.java
+++ is-order-api/src/main/java/com/imooc/security/config/OAuth2WebSecurityConfig.java
@@ -1,18 +1,21 @@
 package com.imooc.security.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationManager;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
-import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.*;
 
 @Configuration
 @EnableWebSecurity
+@AllArgsConstructor
 public class OAuth2WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserDetailsService userDetailsService;
 
     @Bean
     public ResourceServerTokenServices tokenServices() {
@@ -20,7 +23,21 @@ public class OAuth2WebSecurityConfig extends WebSecurityConfigurerAdapter {
         tokenServices.setClientId("orderService");
         tokenServices.setClientSecret("123456");
         tokenServices.setCheckTokenEndpointUrl("http://localhost:9090/oauth/check_token");
+        // 验证资源服务器时将用户信息token转换为User对象
+        tokenServices.setAccessTokenConverter(getAccessTokenConverter());
         return tokenServices;
+    }
+
+    /**
+     * userToken 转换为user对象  转换器
+     * @return
+     */
+    private AccessTokenConverter getAccessTokenConverter() {
+        DefaultAccessTokenConverter accessTokenConverter = new DefaultAccessTokenConverter();
+        DefaultUserAuthenticationConverter userAuthenticationConverter = new DefaultUserAuthenticationConverter();
+        userAuthenticationConverter.setUserDetailsService(userDetailsService);
+        accessTokenConverter.setUserTokenConverter(userAuthenticationConverter);
+        return accessTokenConverter;
     }
 
     @Bean
--- is-server-auth/src/main/java/com/imooc/security/server/auth/OAuth2AuthServerConfig.java
+++ is-server-auth/src/main/java/com/imooc/security/server/auth/OAuth2AuthServerConfig.java
@@ -1,15 +1,22 @@
 package com.imooc.security.server.auth;
 
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 /**
  * OAuth2配置 认证服务器
@@ -19,9 +26,14 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 @AllArgsConstructor
 public class OAuth2AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    private final PasswordEncoder passwordEncoder;
-
     private final AuthenticationManager authenticationManager;
+
+    private final DataSource dataSource;
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
 
     /**
      * 客户端应用详情信息（客户端）
@@ -30,21 +42,23 @@ public class OAuth2AuthServerConfig extends AuthorizationServerConfigurerAdapter
      */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("orderApp")
-                .secret(passwordEncoder.encode("123456"))
-                .scopes("read", "write") // 访问能干什么，是读还是写
-                .accessTokenValiditySeconds(3600) // token失效时间
-                .resourceIds("order-server") // 能访问哪些资源服务器
-                .authorizedGrantTypes("password") // 可以使用哪种授权类型，  一共有四种授权类型
-                .and() // 接着配置一个资源服务器
-
-                .withClient("orderService")
-                .secret(passwordEncoder.encode("123456"))
-                .scopes("read", "write") // 访问能干什么，是读还是写
-                .accessTokenValiditySeconds(3600) // token失效时间
-                .resourceIds("order-server") // 能访问哪些资源服务器
-                .authorizedGrantTypes("password"); // 可以使用哪种授权类型，  一共有四种授权类型
+        // 内存配置客户端
+//        clients.inMemory()
+//                .withClient("orderApp")
+//                .secret(passwordEncoder.encode("123456"))
+//                .scopes("read", "write") // 访问能干什么，是读还是写
+//                .accessTokenValiditySeconds(3600) // token失效时间
+//                .resourceIds("order-server") // 能访问哪些资源服务器
+//                .authorizedGrantTypes("password") // 可以使用哪种授权类型，  一共有四种授权类型
+//                .and() // 接着配置一个资源服务器
+//
+//                .withClient("orderService")
+//                .secret(passwordEncoder.encode("123456"))
+//                .scopes("read", "write") // 访问能干什么，是读还是写
+//                .accessTokenValiditySeconds(3600) // token失效时间
+//                .resourceIds("order-server") // 能访问哪些资源服务器
+//                .authorizedGrantTypes("password"); // 可以使用哪种授权类型，  一共有四种授权类型
+        clients.jdbc(dataSource);
     }
 
     @Override
@@ -55,6 +69,7 @@ public class OAuth2AuthServerConfig extends AuthorizationServerConfigurerAdapter
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         // 负责校验传进来的用户信息是不是合法的
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.tokenStore(tokenStore())
+                 .authenticationManager(authenticationManager);
     }
 }
